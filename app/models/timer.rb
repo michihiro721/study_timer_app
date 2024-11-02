b5b36630f221a22bad66536057f618024a862a81
相良 道広 <sagaramichihiro@sagaramichihironoMacBook-Air.local>
2024-11-02 16:56:43 +0900

    タイマー表示を「x時間x分x秒」で統一した

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -1,17 +1,20 @@
-
 class Timer < ApplicationRecord
-  # 学習時間を計算するメソッド
+  # 学習時間をフォーマットして返すメソッド
   def duration
     if end_time && start_time
-      (end_time - start_time) / 3600.0
+      total_seconds = (end_time - start_time).to_i
+      hours = total_seconds / 3600
+      minutes = (total_seconds % 3600) / 60
+      seconds = total_seconds % 60
+      "#{hours}時間#{minutes}分#{seconds}秒"
     else
-      0
+      "0時間0分0秒"
     end
   end
 
   # 累計学習時間を計算するクラスメソッド
   def self.total_duration
-    all.sum(&:duration)
+    all.sum(&:duration_in_hours)
   end
 
   # 累計学習時間を「xx時間xx分xx秒」の形式で返すメソッド
@@ -21,6 +24,15 @@ class Timer < ApplicationRecord
     minutes = (total_seconds % 3600) / 60
     seconds = total_seconds % 60
     "#{hours}時間#{minutes}分#{seconds}秒"
+  end
+
+  # durationを時間単位の数値で返すヘルパーメソッド
+  def duration_in_hours
+    if end_time && start_time
+      (end_time - start_time) / 3600.0
+    else
+      0
+    end
   end
 
   # 累計時間に応じたスタンプ数を計算するクラスメソッド
